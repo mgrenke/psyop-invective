deafa1a1c7679f3c5b7154106c025668894e189a
Nastia <alekseybogdan@pm.me>
2022-07-12 00:35:33 +0300

    Added basic settings menu

---
+++ ShapeshiftingDetective/Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+    [SerializeField]
+    private TMPro.TMP_Dropdown resolutionDropdown;
+    
+    private Resolution[] _resolutions;
+
+    private void Start()
+    {
+        _resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+        List<String> options = new List<string>();
+
+        var currentResolutionIndex = 0;
+        for (var i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + " x " + _resolutions[i].height;
+            options.Add(option);
+
+            if (_resolutions[i].width == Screen.currentResolution.width 
+                && _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
