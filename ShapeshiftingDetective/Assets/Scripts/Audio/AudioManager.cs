edd4bf7fdcc72429bd0e85abd04d8eb4f6c454c1
Nastia <alekseybogdan@pm.me>
2022-07-12 22:20:10 +0300

    Added sound & audio

---
+++ ShapeshiftingDetective/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+
+        foreach (var s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        var s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+}
